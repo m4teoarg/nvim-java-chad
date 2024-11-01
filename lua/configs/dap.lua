e51b3e885d4c817a66f7ecb87c6ba36434b2c6af
m4teo <m4teoarg@gmail.com>
2024-11-01 13:22:30 -0300

    add icon color dap

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -4,8 +4,26 @@ map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Toggle DAP Br
 
 map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue DAP" })
 
+vim.cmd("hi DapBreakpointColor guifg=#ff5500")
+vim.cmd("hi DapStoppedColor guifg=#417b15")
+
 vim.fn.sign_define(
     "DapBreakpoint",
-    { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+    { text = "", texthl = "DapBreakpointColor", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
 )
+
+vim.fn.sign_define(
+  "DapStopped",
+  { text = "", texthl = "DapStoppedColor", linehl = "DapStopped", numhl = "DapStopped" }
+)
+
+-- vim.fn.sign_define(
+--   "DapLogPoint",
+--   { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+-- )
+--
+-- vim.fn.sign_define(
+--   "DapBreakpointRejected",
+--   { text = "", texthl = "DapBreakpointRejected", linehl = "DapBreakpointRejected", numhl = "DapBreakpointRejected" }
+-- )
 
