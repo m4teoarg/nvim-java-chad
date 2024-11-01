333df3135e61ad6caa5bf2584a6fe24b52712489
m4teo <m4teoarg@gmail.com>
2024-11-01 13:01:56 -0300

    icon dap

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -3,3 +3,9 @@ local map = vim.keymap.set
 map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Toggle DAP Breakpoint" })
 
 map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "Start or continue DAP" })
+
+vim.fn.sign_define(
+    "DapBreakpoint",
+    { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+)
+
