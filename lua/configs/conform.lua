514b604c4dd4591435056f4e91fac6a53a9d22fc
m4teo <m4teoarg@gmail.com>
2024-11-07 12:05:20 -0300

    add google java format

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,20 +1,30 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    ts = { "prettier" },
-    typescript = { "prettier" },
-    python = { "black" },
-    java = { "prettier" },
-  },
+    formatters_by_ft = {
+        lua = { "stylua" },
+        css = { "prettier" },
+        html = { "prettier" },
+        ts = { "prettier" },
+        typescript = { "prettier" },
+        python = { "black" },
+        -- java = { "prettier" },
+        java = {
+            "google-java-format",
+            command = "java",
+            args = {
+                "-jar",
+                vim.fn.expand(
+                    "~/.local/share/nvim/mason/packages/google-java-format/google-java-format-1.24.0-all-deps.jar"
+                ),
+                "-",
+            },
+        },
+    },
 
-
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 5000,
-    lsp_fallback = true,
-  },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 5000,
+        lsp_fallback = true,
+    },
 }
 
 return options
